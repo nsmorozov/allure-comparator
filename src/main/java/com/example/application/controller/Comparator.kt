e2434ec25be892996378d31e8f6c36c8677aef7a
n.morozov <n.morozov@crpt.ru>
2023-12-19 21:11:08 +0600

    feat: implement map compare for 2 reports

--- src/main/java/com/example/application/controller/Comparator.kt
+++ src/main/java/com/example/application/controller/Comparator.kt
@@ -2,6 +2,7 @@ package com.example.application.controller
 
 import com.example.application.dto.AllureDataEntry
 import com.example.application.dto.Diff
+import com.google.common.collect.Maps
 import org.apache.commons.csv.CSVFormat
 import java.io.File
 
@@ -12,34 +13,28 @@ class Comparator {
         val suites2 = readCsvFromFile(path2)
         val s1 = suites1.associate { "${it.classDesc}.${it.methodDesc}" to it.status }
         val s2 = suites2.associate { "${it.classDesc}.${it.methodDesc}" to it.status }
-        val dif = s1 - s2
-
-//        var s1add: Map<String, String> = s1
-//        if (s1.size > s2.size) {
-//            s2.forEach { (k, v) ->
-//               s1add.entries
-//            }
-//        }
-        return Diff(dif, suites1, suites2)
+        val diff = Maps.difference(s1, s2)
+        val d1 = diff.entriesDiffering().mapValues { it.toString() }
+        return Diff(d1, suites1, suites2)
     }
 
     private fun readCsvFromFile(path: String): List<AllureDataEntry> =
             CSVFormat.Builder.create(CSVFormat.DEFAULT).apply {
                 setIgnoreSurroundingSpaces(true)
             }.build().parse(File(path).inputStream().reader())
-                    .drop(1) // drop header
+                    .drop(1) // пропускаем заголовок
                     .map {
-                        AllureDataEntry( status = it[0],
-                                    startTime = it[1],
-                                    stopTime = it[2],
-                                    duration = it[3],
-                                    parentSuite = it[4],
-                                    suite = it[5],
-                                    subSuite = it[6],
-                                    classDesc = it[7],
-                                    methodDesc = it[8],
-                                    name = it[9],
-                                    description = it[10]
+                        AllureDataEntry(status = it[0],
+                                        startTime = it[1],
+                                        stopTime = it[2],
+                                        duration = it[3],
+                                        parentSuite = it[4],
+                                        suite = it[5],
+                                        subSuite = it[6],
+                                        classDesc = it[7],
+                                        methodDesc = it[8],
+                                        name = it[9],
+                                        description = it[10]
                         )
                     }
 }
